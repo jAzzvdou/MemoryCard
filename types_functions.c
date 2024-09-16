5c66e2fb8673d1266f89e24af7a9f0ab024178bf
jazevedo <joaoazevedo592@gmail.com>
2024-09-16 16:54:30 -0300

    Fix: NULL Vector.

--- types_functions.c
+++ types_functions.c
@@ -6,7 +6,7 @@
 /*   By: jazevedo <jazevedo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 18:01:17 by jazevedo          #+#    #+#             */
-/*   Updated: 2024/09/11 11:35:12 by jazevedo         ###   ########.fr       */
+/*   Updated: 2024/09/16 16:14:44 by jazevedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ char	**fillvector(char **vector, size_t memsize)
 	i = 0;
 	while (i < memsize)
 	{
-		vector[i] = type_str(NULL, MALLOC, 1);
+		vector[i] = NULL;
 		i++;
 	}
 	vector[i] = NULL;
